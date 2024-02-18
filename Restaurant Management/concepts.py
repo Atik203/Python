52b848cfc187c557f3cdbbf6f0107cbda496c137
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-02-18 23:00:22 +0600

    create restaurant management oop project

---
+++ Restaurant Management/concepts.py
@@ -0,0 +1,37 @@
+
+"""
+
+Restaurant Management System
+
+Concepts:
+1. inheritance
+2. polymorphism
+3. encapsulation
+4. abstraction
+
+classes:
+1.User: base class (ABC)
+2.Customer(User)
+3.Employee(User)
+4.Manager(Employee) Multi-level inheritance
+5.Chef(Employee)
+6.Cleaner(Employee)
+7.Supplier(User)
+8.Marketer(User)
+
+
+1.FoodItem(ABC)
+2.Drink(FoodItem)
+3.Dessert(FoodItem)
+4.Pizza(FoodItem)
+5.Burger(FoodItem)
+6.Sandwich(FoodItem)
+7.Salad(FoodItem)
+
+
+1.Order
+2.Menu
+3.Restaurant
+
+
+"""
