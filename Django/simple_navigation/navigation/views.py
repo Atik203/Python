fdb5bef099aa4f837cfa275680ce7a6bef261ac9
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2024-03-19 21:22:10 +0600

    template filtering in django

--- Django/simple_navigation/navigation/views.py
+++ Django/simple_navigation/navigation/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def about(request):
     return render(request, 'navigation/about.html')
 def contact(request):
-    return render(request, 'navigation/contact.html')
+    d = {'email': 'test@email.com','birthday':datetime.datetime.now(), 'phone': '123-456-7890','list': [1,2,3],'age': 20, 'name': 'John','courses': [
+        {
+        'id': 1,
+        'name': 'Math',
+    },
+        {
+        'id': 2,
+        'name': 'Science',
+    
+    },
+        {
+        'id': 3,
+        'name': 'English',}],'list2':['python','java','c++']}
+    return render(request, 'navigation/contact.html', d)
